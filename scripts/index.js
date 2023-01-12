48b3fdfbfcecf98f27731792fe9f315184b30455
Macintosh689 <macintosh689@gmail.com>
2023-01-12 22:14:47 +0300

    первое ревью

---
+++ scripts/index.js
@@ -0,0 +1,42 @@
+const popupElement = document.querySelector('.popup');
+const popupCloseButtonElement = popupElement.querySelector('.popup__button-close');
+const popupOpenButtonElement = document.querySelector('.profile__edit-button');
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__name');
+let jobInput = document.querySelector('.popup__description');
+let profileName = document.querySelector('.profile__name');
+let profileDescription = document.querySelector('.profile__description');
+
+
+const openPopup = function () {
+  popupElement.classList.add('popup_opened');
+  nameInput.value = profileName.textContent ;
+  jobInput.value = profileDescription.textContent;
+}
+popupOpenButtonElement.addEventListener('click', openPopup);
+
+
+const closePopup = function () {
+  popupElement.classList.remove('popup_opened');
+}
+popupCloseButtonElement.addEventListener('click', closePopup);
+
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileDescription.textContent = jobInput.value;
+  closePopup();
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
+
+
+
+
+
+
+
+
+
+
