b1a9b4768f064fd52bd2444e31b8e80d5038c729
Macintosh689 <macintosh689@gmail.com>
2023-01-14 19:33:29 +0300

    исправление ошибок после второго ревью

--- scripts/index.js
+++ scripts/index.js
@@ -2,15 +2,15 @@ const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__button-close');
 const popupOpenButtonElement = document.querySelector('.profile__edit-button');
 let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__info_name');
-let jobInput = document.querySelector('.popup__info_description');
+let nameInput = document.querySelector('.popup__info_type_name');
+let jobInput = document.querySelector('.popup__info_type_description');
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
 
 
 const openPopup = function () {
   popupElement.classList.add('popup_opened');
-  nameInput.value = profileName.textContent ;
+  nameInput.value = profileName.textContent;
   jobInput.value = profileDescription.textContent;
 }
 
